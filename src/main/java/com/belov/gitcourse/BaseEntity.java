8afe0b7305d49fe53933757c8e67e04ee483a670
Vladislav Belov <vladik-belov-2015@mail.ru>
2022-05-15 17:12:39 +0400

    create new class BaseEntity

---
+++ src/main/java/com/belov/gitcourse/BaseEntity.java
@@ -0,0 +1,20 @@
+package com.belov.gitcourse;
+
+public class BaseEntity {
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "BaseEntity{" +
+                "id=" + id +
+                '}';
+    }
+}
